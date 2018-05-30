67623042fb3311ee7d0b07a3408dea9b905e5fd2
sitharakannan <sithara.kamalakkannan@gmail.com>
2018-05-29 18:48:42 -0700

    #4 streaming measured and ref forces for 7 muscles

--- iss4/rec_actvns_v6.py
+++ iss4/rec_actvns_v6.py
@@ -9,8 +9,6 @@ import random
 import zmq
 import pickle
 
-ctr = 0
-sine_sum = 0
 
 def initialize_sub_socket(ip, port, topic_filter=b"map"):
     context = zmq.Context()
@@ -38,13 +36,9 @@ reference_M4=[],
 reference_M5=[],
 reference_M6=[]))
 
-fig1 = figure()
-#fig1.multi_line(xs=[[time],[time]], ys=[[measured_M0], [command_M0]], source=source, line_width=2, alpha=0.85, color=['red','blue'])
-# fig2 = Figure()
-# fig2.line(x='time', y='command_M1', source=source, line_width=2, alpha=0.85, color='blue')
-fig1.line(x='time', y='measured_M0', source=source, line_width=2, alpha=0.85, color='red')
-fig1.line(x='time', y='reference_M0', source=source, line_width=2, alpha=0.85, color='blue')
-
+# fig1 = figure()
+# fig1.line(x='time', y='measured_M0', source=source, line_width=2, alpha=0.85, color='red')
+# fig1.line(x='time', y='reference_M0', source=source, line_width=2, alpha=0.85, color='blue')
 
 def update_data():
     global socket
@@ -53,33 +47,41 @@ def update_data():
         #print("VALUES", msg)
         message = (pickle.loads(msg, encoding="latin1"))
         #print("values", message)
-        #[forces, targetforces, commands] = message
         measuredForces = message[0][0]
         referenceForces = message[0][1]
         #residualForces = [message[0][0][i]-message[0][1][i] for i in range(7)]
         commands = message[0][2]
         timestamp = message[1]
-        # print("FORCES",message[0][0])
-        # print("TARGETFORCES",message[0][1])
-        # print("COMMANDS",message[0][2])
-        # print("TIMESTAMP", message[1])
-        # for i in range(7):
-        #     "measured_M%s"%i = measuredForces[i]
-        #     ("command_M%s"%i) = commands[i]
 
         new_data = dict(time=[timestamp],measured_M0=[measuredForces[0]],measured_M1=[measuredForces[1]],measured_M2=[measuredForces[2]],measured_M3=[measuredForces[3]],measured_M4=[measuredForces[4]],measured_M5=[measuredForces[0]],measured_M6=[measuredForces[6]],reference_M0=[referenceForces[0]],reference_M1=[referenceForces[1]],reference_M2=[referenceForces[2]],reference_M3=[referenceForces[3]],reference_M4=[referenceForces[4]],reference_M5=[referenceForces[5]],reference_M6=[referenceForces[6]])
 
         source.stream(new_data, 100)
-        #print(source.data['measuredForces'])
 
     except KeyboardInterrupt:
         socket.close()
+
+def update():
+    global main_layout
+    for i in range(7):
+        main_layout.children[i] = create_figline()[i]
+
+def create_figline():
+    global source
+    update_data()
+    colors = ["#762a83","#76EEC6","#53868B","#FF1493","#ADFF2F","#292421","#FFE1FF"]
+    figlist = []
+    for muscle_index in range(7):
+        fig = figure(plot_width=250, plot_height=250, title=None)
+        fig.line(source=source, x='time', y='measured_M%s' %muscle_index, line_width=2, alpha=0.85, color=colors[muscle_index])
+        fig.line(source=source, x='time', y='reference_M%s' %muscle_index, line_width=2, alpha=0.85, color='blue')
+        figlist.append(fig)
+    return figlist
 
 ip = '127.0.0.1'
 port = '12345'
 socket = initialize_sub_socket(ip, port)
 
-curdoc().add_periodic_callback(update_data, 1)
-curdoc().add_root(fig1)
 
-#cProfile.run(re.compile("curdoc().add_periodic_callback(update_data, 1)"))
+curdoc().add_periodic_callback(update, 1)
+main_layout = row(create_figline())
+curdoc().add_root(main_layout)
