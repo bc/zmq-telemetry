771689378e7d380f9b1ead6dee4e45dfeeaf9231
sitharakannan <sithara.kamalakkannan@gmail.com>
2018-06-04 12:13:58 -0700

    added timestamps for received msgs

--- iss4/push_server.py
+++ iss4/push_server.py
@@ -7,17 +7,14 @@ def server_push(port="5556"):
     context = zmq.Context()
     socket = context.socket(zmq.REP)
     socket.bind("tcp://*:%s" % port)
-    print "Running server on port: ", port
+    print("Running server on port: ", port)
     while True:
         message = socket.recv()
-        print "Received request: ", message
+        print("Received referenceForce @ %s" % (message).decode("utf-8"))
         referenceForces = []
         referenceForces.append([random.uniform(0,1) for i in range(7)])
-        #print("Continue")
         pickled_contents = pickle.dumps(referenceForces)
         socket.send(pickled_contents)
-        #msg = socket.recv()
-        #print(msg)
         time.sleep(8)
 
 server_push()
