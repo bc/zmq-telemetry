776ec80216308dbd0f3950432871e8e7edfd08cc
sitharakannan <sithara.kamalakkannan@gmail.com>
2018-06-05 11:55:24 -0700

    refClient for sp18

---
+++ iss4/refClient.py
@@ -0,0 +1,21 @@
+import zmq
+import time
+import random
+import pickle
+
+def push_randomRefForce(port="5558"):
+    context = zmq.Context()
+    socket = context.socket(zmq.REP)
+    socket.bind("tcp://*:%s" % port)
+    print("Running server on port: ", port)
+    while True:
+        message = socket.recv()
+        print("Received : %s" % message) #(message).decode("utf-8")
+        referenceForces = []
+        randomRef = random.uniform(0,1)
+        referenceForces.append([randomRef for i in range(7)])
+        pickled_contents = pickle.dumps(referenceForces)
+        socket.send(pickled_contents)
+        time.sleep(10)
+
+push_randomRefForce()
