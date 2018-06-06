d2777e7630105a2a39298bcf22b3cbce5ebc3461
sitharakannan <sithara.kamalakkannan@gmail.com>
2018-06-06 13:00:25 -0700

    #4 removed socket for server & unnecessary print stmts

--- subscriber_timeseries/recv_actns.py
+++ subscriber_timeseries/recv_actns.py
@@ -11,7 +11,19 @@ import pickle
 import pdb
 import time
 
-def initialize_sub_socket(ip, port_sub, port_serv, topic_filter=b"map"):
+rpi_emulator = True
+brian = False
+
+if rpi_emulator:
+    ip = '127.0.0.1'
+elif brian:
+    ip = '169.254.12.240'
+else:
+    ip = '10.42.0.82'
+
+port_sub = '12345'
+
+def initialize_sub_socket(ip, port_sub, topic_filter=b"map"):
     context = zmq.Context()
     socket_sub = context.socket(zmq.SUB)
     socket_string = "%s:%s" % (ip, port_sub)
@@ -21,12 +33,7 @@ def initialize_sub_socket(ip, port_sub, port_serv, topic_filter=b"map"):
     socket_sub.setsockopt(zmq.SUBSCRIBE, topic_filter)
     print('Set ZMQ Subscriber with topic filter')
 
-    socket_pull = context.socket(zmq.REQ)
-    socket_pull.connect ("tcp://localhost:%s" % port_serv)
-    print("Connected to server with port %s" % port_serv)
-    print("Sending dummy request")
-    socket_pull.send_string("Hello")
-    return(socket_sub, socket_pull)
+    return(socket_sub)
 
 source = ColumnDataSource(dict(time=[], measured_M0=[],
 measured_M1=[],
@@ -43,45 +50,29 @@ reference_M4=[],
 reference_M5=[],
 reference_M6=[]))
 
-i = 0
 referenceForces = []
 ref_flag = True
 def update_data():
-    global socket_sub, socket_pull, i, ref_flag, referenceForces
-    i = i+1
-    #print("IN UPDATE DATA",i+1)
+    global socket_sub
     try:
         #print(socket_sub, socket_pull)
         poller = zmq.Poller()
         poller.register(socket_sub, zmq.POLLIN)
-        poller.register(socket_pull, zmq.POLLIN)
         socks = dict(poller.poll())
 
-        if socket_pull in socks and socks[socket_pull] == zmq.POLLIN:
-            print("PULLING")
-            msg = socket_pull.recv()
-            new_referenceForce = (pickle.loads(msg, encoding="latin1"))
-            referenceForces = new_referenceForce[0]
-            print("## PULL ##",referenceForces)
-            ref_flag = False
-            print("Recieved control command: %s" % referenceForces)
-            socket_pull.send_string(str(time.time()))
-            #socket_pull.send("Received reference Force")
-
         if socket_sub in socks and socks[socket_sub] == zmq.POLLIN:
-            print("PUBBING")
             [topic, msg] = socket_sub.recv_multipart()
             #print("VALUES", msg)
             message = (pickle.loads(msg, encoding="latin1"))
             print("values", message)
             measuredForces = message[0][0]
-            if ref_flag:
-                referenceForces = message[0][1]
+            referenceForces = message[0][1]
             #residualForces = [message[0][0][i]-message[0][1][i] for i in range(7)]
             commands = message[0][2]
             timestamp = message[1]
             print("%s"%(str(time.time()-timestamp)))
-            print("## PUBB ##",referenceForces)
+            #print("## PUBB ##",referenceForces)
+            #TODO create a function that returns measured and ref Forces in proper fmt
             new_data = dict(time=[timestamp],measured_M0=[measuredForces[0]],measured_M1=[measuredForces[1]],measured_M2=[measuredForces[2]],measured_M3=[measuredForces[3]],measured_M4=[measuredForces[4]],measured_M5=[measuredForces[0]],measured_M6=[measuredForces[6]],reference_M0=[referenceForces[0]],reference_M1=[referenceForces[1]],reference_M2=[referenceForces[2]],reference_M3=[referenceForces[3]],reference_M4=[referenceForces[4]],reference_M5=[referenceForces[5]],reference_M6=[referenceForces[6]])
 
             source.stream(new_data, 100)
@@ -107,17 +98,9 @@ def create_figline():
         figlist.append(fig)
     return figlist
 
-ip = '127.0.0.1'
-brian = False
-if brian:
-    ip = '169.254.12.240'
 
-#ip = '10.42.0.82'
-port_sub = '12345'
-port_serv = '5556'
-socket_sub, socket_pull = initialize_sub_socket(ip, port_sub, port_serv)
-
+socket_sub = initialize_sub_socket(ip, port_sub, port_serv)
 
 curdoc().add_periodic_callback(update, 1)
-main_layout = column(create_figline(), sizing_mode='scale_width')
+main_layout = column(create_figline())
 curdoc().add_root(main_layout)
