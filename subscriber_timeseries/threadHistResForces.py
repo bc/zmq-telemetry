2f4c61aef92a257af60039a1708c71843fd7742a
sitharakannan <sithara.kamalakkannan@gmail.com>
2018-06-18 18:16:14 -0700

    #7 cleaned code with functions

--- subscriber_timeseries/threadHistResForces.py
+++ subscriber_timeseries/threadHistResForces.py
@@ -41,8 +41,25 @@ def update(plotData,residualForces):
     source_forces.stream(residualForces,100)
     source_plot.stream(plotData,100)
 
-hist = []
-edges = []
+def modify_to_plot():
+    hist = []
+    edges = []
+    for i in range(7):
+        histogram  = np.histogram(source_forces.data['residual_M%s'%i])
+        for j in range(len(histogram[1])):
+            histogram[1][j] = (histogram[1][j] + i*1)
+        print("histogram[1]",histogram[1])
+        hist.append(histogram[0])
+        edges.append(histogram[1])
+    return(hist,edges)
+
+def compose_column_data_source_entry(timestamp, residualForces, hist, edges):
+    residualForces = dict(time=[timestamp],residual_M0=[residualForces[0]],residual_M1=[residualForces[1]],residual_M2=[residualForces[2]],residual_M3=[residualForces[3]],residual_M4=[residualForces[4]],residual_M5=[residualForces[5]],residual_M6=[residualForces[6]])
+
+    plotData =  dict(hist_M0=hist[0].tolist(),hist_M1=hist[1].tolist(),hist_M2=hist[2].tolist(),hist_M3=hist[3].tolist(),hist_M4=hist[4].tolist(),hist_M5=hist[5].tolist(),hist_M6=hist[6].tolist(),ledges_M0=edges[0][:-1].tolist(),ledges_M1=edges[1][:-1].tolist(),ledges_M2=edges[2][:-1].tolist(),ledges_M3=edges[3][:-1].tolist(),ledges_M4=edges[4][:-1].tolist(),ledges_M5=edges[5][:-1].tolist(),ledges_M6=edges[6][:-1].tolist(),redges_M0=edges[0][1:].tolist(),redges_M1=edges[1][1:].tolist(),redges_M2=edges[2][1:].tolist(),redges_M3=edges[3][1:].tolist(),redges_M4=edges[4][1:].tolist(),redges_M5=edges[5][1:].tolist(),redges_M6=edges[6][1:].tolist())
+
+    return(residualForces, plotData)
+
 def subscribe_and_stream():
     global fig
     while True:
@@ -55,38 +72,22 @@ def subscribe_and_stream():
             if socket_sub in socks and socks[socket_sub] == zmq.POLLIN:
                 [topic, msg] = socket_sub.recv_multipart()
                 message = (pickle.loads(msg, encoding="latin1"))
-                #print("values", message)
+
                 measuredForces = message[0][0]
                 referenceForces = message[0][1]
                 ### using absolute difference ###
                 residualForces = [np.abs(message[0][0][i]-message[0][1][i]) for i in range(7)]
                 commands = message[0][2]
                 timestamp = message[1]
-                #print("residualForces", residualForces)
-                hist = []
-                edges = []
-                for i in range(7):
-                    histogram  = np.histogram(source_forces.data['residual_M%s'%i])
-                    for j in range(len(histogram[1])):
-                        histogram[1][j] = (histogram[1][j] + i*1)
-                    print("histogram[1]",histogram[1])
-                    hist.append(histogram[0])
-                    edges.append(histogram[1])
 
-                residualForces = dict(time=[timestamp],residual_M0=[residualForces[0]],residual_M1=[residualForces[1]],residual_M2=[residualForces[2]],residual_M3=[residualForces[3]],residual_M4=[residualForces[4]],residual_M5=[residualForces[5]],residual_M6=[residualForces[6]])
-
-                plotData =  dict(hist_M0=hist[0].tolist(),hist_M1=hist[1].tolist(),hist_M2=hist[2].tolist(),hist_M3=hist[3].tolist(),hist_M4=hist[4].tolist(),hist_M5=hist[5].tolist(),hist_M6=hist[6].tolist(),ledges_M0=edges[0][:-1].tolist(),ledges_M1=edges[1][:-1].tolist(),ledges_M2=edges[2][:-1].tolist(),ledges_M3=edges[3][:-1].tolist(),ledges_M4=edges[4][:-1].tolist(),ledges_M5=edges[5][:-1].tolist(),ledges_M6=edges[6][:-1].tolist(),redges_M0=edges[0][1:].tolist(),redges_M1=edges[1][1:].tolist(),redges_M2=edges[2][1:].tolist(),redges_M3=edges[3][1:].tolist(),redges_M4=edges[4][1:].tolist(),redges_M5=edges[5][1:].tolist(),redges_M6=edges[6][1:].tolist())
-
-                #print(messagedata)
-
+                hist,edges = modify_to_plot()
+                residualForces, plotData = compose_column_data_source_entry(timestamp, residualForces, hist, edges)
                 doc.add_next_tick_callback(partial(update,plotData,residualForces))
 
         except KeyboardInterrupt:
             print("CLEAN UP CLEAN UP EVERYBODY CLEANUP")
             while not socket_sub.closed:
                 make_clean_exit(socket_sub)
-
-
 
 colors = ["#762a83", "#76EEC6", "#53868B",
           "#FF1493", "#ADFF2F", "#292421", "#EE6A50"]
